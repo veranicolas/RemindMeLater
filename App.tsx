b629a41fe6a1cfc48dc1235056fd42d6ea36493c
Nicolas Vera <nicolasveraa97@gmail.com>
2022-08-30 04:06:33 -0300

    Initial commit

---
+++ App.tsx
@@ -0,0 +1,12 @@
+import React, {type PropsWithChildren} from 'react';
+import { Main } from './src/Main';
+
+const App = () =>{
+
+  return (
+    <Main />
+  );
+};
+
+
+export default App;
---
+++ src/Main.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import { View, SafeAreaView, Text, StyleSheet } from 'react-native'
+
+const Main = () =>{
+
+    return (
+        <SafeAreaView>
+            <View>
+                <Text>Hola si nene</Text>
+            </View>
+        </SafeAreaView>
+    );
+};
+
+const styles = StyleSheet.create({
+
+});
+
+export { Main }
