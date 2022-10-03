d06352439bf03e1cce0ce3ba37f61f6e030dc45c
Nicolas Vera <nicolasveraa97@gmail.com>
2022-10-03 08:11:55 -0300

    Add async storage for data persistence

--- src/jotai/atoms.ts
+++ src/jotai/atoms.ts
@@ -1,4 +1,5 @@
 import { atom } from "jotai";
+import AsyncStorage from '@react-native-async-storage/async-storage';
 
 type TaskAtom = {
     id:number,
@@ -9,8 +10,28 @@ type TaskAtom = {
 
 type Theme = 'light' | 'dark'
 
-const taskAtoms = atom<TaskAtom[]>([])
-const theme = atom<Theme>('light')
-const deleteMode = atom<boolean>(false)
+const atomWithAsyncStorage = (key:string, initialValue:any) => {
+    const baseAtom = atom(initialValue)
+    baseAtom.onMount = (setValue) => {
+      ;(async () => {
+        const item = await AsyncStorage.getItem(key)
+        setValue(JSON.parse(item || '[]'))
+      })()
+    }
+    const derivedAtom = atom(
+      (get) => get(baseAtom),
+      (get, set, update) => {
+        const nextValue =
+          typeof update === 'function' ? update(get(baseAtom)) : update
+        set(baseAtom, nextValue)
+        AsyncStorage.setItem(key, JSON.stringify(nextValue))
+      }
+    )
+    return derivedAtom
+}
+
+const taskAtoms = atomWithAsyncStorage('tasks',[])
+const theme = atomWithAsyncStorage('theme','light')
+const deleteMode = atomWithAsyncStorage('deleteMode',false)
 
 export { taskAtoms, theme, deleteMode }
